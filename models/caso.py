cef948e26d4ac6824b3a4f39fbf4c2080b120e7d
Fei Manheche <feimanheche@robobo.org>
2023-03-30 22:50:03 -0400

    add initial persons involved to case list

--- models/caso.py
+++ models/caso.py
@@ -2,16 +2,18 @@ from odoo import api, fields, models
 from odoo.exceptions import UserError
 import uuid
 
+
 class Caso(models.Model):
     _name = "linhafala.caso"
     _description = "Formulário de Caso linha fala criança"
     _inherit = [
-        'mail.thread', 
+        'mail.thread',
         'mail.activity.mixin'
-        ]
-    
+    ]
+
     case_id = fields.Char(string="Id do caso", readonly=True)
-    call_id = fields.Many2one(comodel_name='linhafala.chamada', string="Chamada")
+    call_id = fields.Many2one(
+        comodel_name='linhafala.chamada', string="Chamada")
     case_status = fields.Selection(
         string='Estado do caso',
         selection=[
@@ -19,7 +21,7 @@ class Caso(models.Model):
             ("Dentro do sistema", "Dentro do sistema"),
             ("Aberto/Pendente", "Aberto/Pendente"),
             ("No Arquivo Morto", "No Arquivo Morto"),
-            ("Assistido","Assistido")
+            ("Assistido", "Assistido")
         ],
         help="Estado do caso"
     )
@@ -56,21 +58,29 @@ class Caso(models.Model):
         default="Escola",
         help="Local de Ocorrencia"
     )
-    detailed_description = fields.Html(string='Descrição detalhada', attrs={'style': 'height: 500px;'})
-    case_type = fields.Many2one(comodel_name='linhafala.caso.categoria', string="Categoria")
-    secundary_case_type = fields.Many2one(comodel_name='linhafala.caso.subcategoria', string="Subcategoria")
-    case_type_classification = fields.Many2one(comodel_name='linhafala.caso.case_type_classification', string="Classificaçäo Provisória")
+    detailed_description = fields.Html(string='Descrição detalhada', attrs={
+                                       'style': 'height: 500px;'})
+    case_type = fields.Many2one(
+        comodel_name='linhafala.caso.categoria', string="Categoria")
+    secundary_case_type = fields.Many2one(
+        comodel_name='linhafala.caso.subcategoria', string="Subcategoria")
+    case_type_classification = fields.Many2one(
+        comodel_name='linhafala.caso.case_type_classification', string="Classificaçäo Provisória")
 
-
-    reporter_by = fields.Many2one('res.users', string='Gestão', default=lambda self: self.env.user, readonly=True)
-    created_at = fields.Datetime(string='Data de criaçäo', default=lambda self: fields.Datetime.now(), readonly=True)
-    updated_at = fields.Datetime(string='Data de actualizaçäo', default=lambda self: fields.Datetime.now(), readonly=True)
-    created_by = fields.Many2one('res.users', string='Criado por', default=lambda self: self.env.user, readonly=True)
+    reporter_by = fields.Many2one(
+        'res.users', string='Gestão', default=lambda self: self.env.user, readonly=True)
+    created_at = fields.Datetime(
+        string='Data de criaçäo', default=lambda self: fields.Datetime.now(), readonly=True)
+    updated_at = fields.Datetime(string='Data de actualizaçäo',
+                                 default=lambda self: fields.Datetime.now(), readonly=True)
+    created_by = fields.Many2one(
+        'res.users', string='Criado por', default=lambda self: self.env.user, readonly=True)
     is_deleted = fields.Boolean(string='Apagado', default=False, readonly=True)
     uuid = fields.Char(string='UUID', readonly=True)
     is_locked = fields.Boolean(string='Is Locked', default=False)
     lock_date = fields.Datetime()
-
+    persons_involved_line_ids = fields.One2many('linhafala.caso.person_involved', 'fullname',
+                                                string="Person Involved lines")
 
     _sql_constraints = [
         ('unique_case_id', 'unique(case_id)', 'The case_id must be unique'),
@@ -80,23 +90,24 @@ class Caso(models.Model):
         if vals:
             vals['updated_at'] = fields.Datetime.now()
         return super(Caso, self).write(vals)
-    
+
     def unlink(self):
         for record in self:
             record.write({'is_deleted': True})
         return super(Caso, self).unlink()
-    
+
     @api.model
     def create(self, vals):
         vals['uuid'] = str(uuid.uuid4())
         return super(Caso, self).create(vals)
-    
+
     @api.model
     def create(self, vals):
         if vals.get('case_id', '/') == '/':
-            vals['case_id'] = self.env['ir.sequence'].next_by_code('linhafala.chamada.case_id.seq') or '/'
+            vals['case_id'] = self.env['ir.sequence'].next_by_code(
+                'linhafala.chamada.case_id.seq') or '/'
         return super(Caso, self).create(vals)
-    
+
     # Lock the case for single user edit
     # TODO: Check and fix that a record gets created and managed by a single user!
     @api.model
@@ -104,36 +115,37 @@ class Caso(models.Model):
         # Set the user_id to the current user
         vals['user_id'] = self.env.user.id
         return super(Caso, self).create(vals)
-    
+
     def write(self, vals):
         if 'is_locked' in vals:
             # Check if the record is already locked by someone else
             if self.is_locked and self.user_id != self.env.user:
-                raise UserError('This record is locked by {}.'.format(self.user_id.name))
-            
+                raise UserError(
+                    'This record is locked by {}.'.format(self.user_id.name))
+
             # Set the lock_date and user_id fields when the record is locked
             if vals['is_locked']:
                 vals['lock_date'] = fields.Datetime.now()
                 vals['user_id'] = self.env.user.id
-                
+
             # Unlock the record when is_locked is set to False
             else:
                 vals['lock_date'] = False
                 vals['user_id'] = False
-                
+
         return super(Caso, self).write(vals)
-    
-    # TODO: Review cascade select or remove this field, replacing with buttons as with the current app workflow    
+
+    # TODO: Review cascade select or remove this field, replacing with buttons as with the current app workflow
     @api.onchange('case_type')
     def _case_type_onchange(self):
         for rec in self:
             return {'value': {'secundary_case_type': False}, 'domain': {'secundary_case_type': [('categoria_id', '=', rec.case_type.id)]}}
-    
+
     @api.onchange('secundary_case_type')
     def _secundary_case_type_onchange(self):
         for rec in self:
             return {'value': {'case_type_classification': False}, 'domain': {'case_type_classification': [('categoria_id', '=', rec.secundary_case_type.id)]}}
-        
+
     @api.model
     def _register_hook(self):
         # Register the new sequence
@@ -144,7 +156,7 @@ class Caso(models.Model):
             'padding': 4,
         })
         return super(Caso, self)._register_hook()
-    
+
     def action_confirm(self):
         self.callcaseassistance_status = 'Aberto/Pendente'
 
@@ -156,3 +168,104 @@ class Caso(models.Model):
 
     def action_cancel(self):
         self.callcaseassistance_status = 'Encerrado'
+
+
+class PersonInvolved(models.Model):
+    _name = "linhafala.caso.person_involved"
+    _description = "Person Involved Lines"
+
+    fullname = fields.Char(string="Nome completo", required=True)
+    id_number = fields.Selection(
+        string='Tipo de Identificação',
+        selection=[
+            ("BI", "BI"),
+            ("NUIT", "NUIT"),
+            ("Cartão de Eleitor", "Cartão de Eleitor"),
+            ("Cedula pessoal", " Cedula pessoal"),
+            ("Certidão de Nascimento", " Certidão de Nascimento"),
+            ("Carta de condução", "Carta de condução"),
+            ("Outro", "Outro"),
+        ],
+        help="Tipo de documento de identificação"
+    )
+    nr_identication = fields.Char(string="Numero de Identificação")
+    wants_to_be_annonymous = fields.Boolean("Deja permanecer Anónimo")
+    person_type = fields.Selection(
+        string='Categoria',
+        selection=[
+            ("Contactante", "Contactante"),
+            ("Contactante+Vítima", "Contactante+Vítima"),
+            ("Vítima", "Vítima"),
+            ("Perpetrador", "Perpetrador"),
+        ],
+        help="Categoria"
+    )
+    contact = fields.Char(string="Contacto")
+    alternate_contact = fields.Char(string="Contacto Alternativo")
+    provincia = fields.Many2one(
+        comodel_name='linhafala.provincia', string="Provincia")
+    distrito = fields.Many2one(
+        comodel_name='linhafala.distrito', string="Districto")  # ,
+    #    domain=lambda self: [('provincia', '=', self._compute_allowed_distrito_values())])
+    bairro = fields.Char(string="Bairro")
+    living_relatives = fields.Selection(
+        string='Com quem vive?',
+        selection=[
+            ("Não aplicavél", "Não aplicavél"),
+            ("Outra situação", "Outra situação"),
+            ("No Centro", "No Centro"),
+            ("Sozinho(a)", "Sozinho(a)"),
+            ("Com os tios maternos", "Com os tios maternos"),
+            ("Com os tios paternos", "Com os tios paternos"),
+            ("Só com a mae", "Só com a mae"),
+            ("Só com o pai", "Só com o pai"),
+            ("Só com os irmãos", "Só com os irmãos"),
+            ("Com a familia adoctiva", "Com a familia adoctiva"),
+            ("Familia toda", "Familia toda"),
+            ("Avo", "Avo")
+        ],
+        help="Com quem vive?"
+    )
+    victim_relationship = fields.Selection(
+        string='Relação com a(s) vítima(s):',
+        selection=[
+            ("Mentora", "Mentora"),
+            ("Não aplicavél", "Não aplicavél"),
+            ("Denunciante", "Denunciante"),
+            ("Vitima", "Vitima"),
+            ("Nenhuma", "Nenhuma"),
+            ("Outros", "Outros"),
+            ("Colega", "Colega"),
+            ("Primo(a)", "Primo(a)"),
+            ("Esposo", "Esposo"),
+            ("Namorado", "Namorado"),
+            ("Amigo", "Amigo"),
+            ("Educador(a)", "Educador(a)"),
+            ("Professor(a)", "Professor(a)"),
+            ("Empregador", "Empregador"),
+            ("Irmã(o)", "Irmã(o)"),
+            ("Avo", "Avo"),
+            ("Vizinho (a)", "Vizinho (a)"),
+            ("Madrasta", "Madrasta"),
+            ("Padrasto", "Padrasto"),
+            ("Tio(a)", "Tio(a)"),
+            ("Pai", "Pai"),
+            ("Mãe", "Mãe"),
+        ],
+        help="Relação com a(s) vítima(s):"
+    )
+    gender = fields.Selection(
+        string='Sexo',
+        selection=[
+            ("male", "Masculino"),
+            ("female", "Feminino"),
+            ("other", "Desconhecido"),
+        ],
+        help="Sexo"
+    )
+    age = fields.Selection([(str(i), str(i)) for i in range(6, 81)] + [('81+', '81+')],
+                           string='Idade')
+    on_school = fields.Boolean("Estuda?")
+    grade = fields.Selection([(str(i), str(i)) for i in range(0, 12)]
+                             + [('Ensino Superior', 'Ensino Superior')],
+                             string='Classe')

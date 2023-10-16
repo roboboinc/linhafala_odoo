c0cf7435a81bbd3d982cbd7acc3e2c599d57662a
darkeny <darkenpaulo00@gmail.com>
2023-10-16 08:08:56 +0200

    fix: resolve bug

--- models/__init__.py
+++ models/__init__.py
@@ -13,4 +13,6 @@ from . import moz_learning
 from . import moz_learning_complaint_details
 from . import gender_based_violence
 from . import deficiente
-
+from . import agent
+from . import asterisk
+from . import popup

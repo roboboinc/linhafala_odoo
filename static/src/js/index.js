e9aacca683935713400fc64d3da8b66b687e8873
Shelton Muianga <muiangashelton472@gmail.com>
2023-10-30 09:36:40 +0200

    passar o parametro para o formulario

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -4,28 +4,38 @@ odoo.define('linhafala_odoo.call_popup', function (require) {
     const socket = io('http://localhost:3001');
     let isDisplayed = false;
     let dialogInstance = null;
+    let extensionAnswered = false;
 
     socket.addEventListener('message', message => {
         const events = message;
 
-        if ((events.Event === 'Varset' || events.ChannelStateDesc === 'Ring') && !isDisplayed) {
-            display( events.$time, events.CallerIDNum, events.ConnectedLineNum);
-            isDisplayed = true;
-
-            if (events.Event === 'Hangup'){
-                dialogInstance.close()
+        if (!extensionAnswered) {
+            if (events.Event === 'Varset' || events.ChannelStateDesc === 'Ring') {
+                display();
+                isDisplayed = true;
+            }
+    
+            if (events.Event === 'Hangup') {
+                dialogInstance.close();
+            }
+    
+            if (events.Event === 'Bridge' && events.BridgeState === 'Link') {
+                extensionAnswered = true;
             }
         }
     });
 
-    const display = (time, callerIDNum, connectedLineNum) => {
-        var Dialog = require('web.Dialog');
-        const title = "Recebendo chamada...";
+    var Dialog = require('web.Dialog');
+    const title = "Recebendo chamada...";
+    const connectedLineNum = "000";
+    const callerIDNum ="123";
+    const time = "2013-";
 
+    setTimeout(() => {
         try {
             dialogInstance = Dialog.confirm(
             this,
-                 `Time: ${time}\nCaller: ${callerIDNum}\nConnected: ${connectedLineNum}`,
+                `Time: ${time}\nCaller: ${callerIDNum}\nConnected: ${connectedLineNum}`,
             {
                 title: title,
 
@@ -33,15 +43,23 @@ odoo.define('linhafala_odoo.call_popup', function (require) {
                     console.log("Click Close");
                 },
                 confirm_callback: function () {
-                    window.location.href = '/web#cids=1&menu_id=110&action=155&model=linhafala.chamada&view_type=form';
+                    const connectedLineNumParam = encodeURIComponent(connectedLineNum);
+                    const url = `/web#cids=1&menu_id=110&action=155&model=linhafala.chamada&view_type=form&contact=${connectedLineNumParam}`;
+                
+                    window.location.href = url;
+
+                    $(".some_class").text(this.connectedLineNumParam);
                 },
+                           
                 cancel_callback: function () {
                     console.log("Click Cancel");
-                }
+                },
+                
             }
         );
         } catch (error) {
             console.error("An error occurred:", error);
         }
-    }
+    }, 5000);
+
 });

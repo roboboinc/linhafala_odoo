93d0890f12a8e1c40baf024a92412f81e84cc716
Fei Manheche <feimanheche@robobo.org>
2025-12-03 01:08:05 +0200

    reorder manifest items

--- __manifest__.py
+++ __manifest__.py
@@ -22,13 +22,13 @@
         'views/linhafala_cases_referral.xml',
         # Ensure defect/related actions load before menus
         'views/linhafala_deficiente_views.xml',
+        'views/linhafala_knowledgebase_views.xml',
         # Menus (require actions to exist)
         'views/linhafala_menus.xml',
         # Export wizards and other views that may reference menus/actions
         'views/export_wizard_views.xml',
         'views/linhafala_case_form_view.xml',
         'views/linhafala_configurations_views.xml',
-        'views/linhafala_knowledgebase_views.xml',
         'views/linhafala_person_involved.xml',
         'views/linhafala_dashboard_manager.xml',
         'views/linhafala_dashboard_home.xml',
